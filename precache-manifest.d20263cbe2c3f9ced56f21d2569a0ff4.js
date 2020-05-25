229c5f6d6a42c8df595b142c2a119bb28e85c9ec
githubrohan <rohan.gkp.786@gmail.com>
2020-05-25 20:42:38 +0530

    Updates

---
+++ precache-manifest.d20263cbe2c3f9ced56f21d2569a0ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb01e763a40c93b7d5a9f96f1d9e947a",
+    "url": "/React-todo-App/index.html"
+  },
+  {
+    "revision": "e996ff37ab7e7aa1d9fc",
+    "url": "/React-todo-App/static/css/main.3a8bd2a8.chunk.css"
+  },
+  {
+    "revision": "7b1a4244da34f792a134",
+    "url": "/React-todo-App/static/js/2.150bdffc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-App/static/js/2.150bdffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e996ff37ab7e7aa1d9fc",
+    "url": "/React-todo-App/static/js/main.1a11674e.chunk.js"
+  },
+  {
+    "revision": "e2460fdc3bf99f18bbda",
+    "url": "/React-todo-App/static/js/runtime-main.e14fd501.js"
+  }
+]);
